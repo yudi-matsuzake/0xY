25ff63b9de30ca2094756cf8bfce624f9c2a17c0
Yudi Matsuzake <yudi.matsuzake@gmail.com>
2015-10-06 00:33:00 -0300

    0x6962616269647579

---
+++ 0xY.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+#define _1 *8
+int main(){enum x{__,___,____,_____};unsigned long long int _=0x79UL<<__ _1|0x75UL<<___ _1|0x64UL<<____ _1|0x69UL<<_____ _1;printf("%s\n",((char*)&_));return 0;}
